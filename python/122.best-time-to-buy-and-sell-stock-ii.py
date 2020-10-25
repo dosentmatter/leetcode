03bf1de279c9658b63160b469b21e5d3a5f9b117
Kevin Lau <kelau1993@gmail.com>
2020-10-24 21:52:41 -0700

    Remove trailing whitespace

--- python/122.best-time-to-buy-and-sell-stock-ii.py
+++ python/122.best-time-to-buy-and-sell-stock-ii.py
@@ -8,12 +8,12 @@ class Solution:
         """
         Same as `maxProfit1()` but doesn't use itertools.
         """
-        
+
         profit = 0
 
         for i in range(1, len(prices)):
             if prices[i - 1] < prices[i] :
-                profit += prices[i] - prices[i - 1] 
+                profit += prices[i] - prices[i - 1]
 
         return profit
 
@@ -21,10 +21,10 @@ class Solution:
         """
         Leetcode optimal solution. Pretend you can buy-sell on the same day because
         the sum of profits of the small hills is equal to the profit of the full hill.
-        
+
         Use itertools.
         """
-        
+
         profit = 0
 
         for previous_price, current_price in zip(
@@ -35,7 +35,7 @@ class Solution:
                 profit += current_price - previous_price
 
         return profit
-    
+
     def maxProfit0(self, prices: List[int]) -> int:
         """
         Use the greedy approach - buy low and sell high at the ends of increasing subsequences.
@@ -44,11 +44,11 @@ class Solution:
         allows previous_element <= current_element
         ```
         Note the less than or EQUAL to.
-        
+
         The reasoning for this is because if you have an increasing subsequence, the max profit
         you can make from that subsequence is from buying and selling at the ends and not
         partitioning up the subsequence in multiple buy-sell pairs.
-        
+
         Here is visual reasoning:
         Below is a subsequence somewhere in the middle of `prices`. The line represents a linearly
         increasing axis of price. So the increase in price `a - b` is less than `c - b`, since
@@ -65,13 +65,13 @@ class Solution:
         current_high - b # can't `- a` because sold `a` above so can't buy `a` on same day
         ```
         Then you are missing profit from `b - a`
-        
+
         You are better off doing `current_high - current_low` and getting the full profit of the
         subsequence.
-        
+
         The solution is to keep track of the current low and high of each increasing subsequence
         and buy on the low, sell on the high, and skip the intermediate increasing days.
-        
+
         Earlier, I said I allow my increasing subsequences to be less than or EQUAL to.
         Here is an explanation why that is allowed.
 
@@ -79,7 +79,7 @@ class Solution:
                       a
          current_low  b  c      d   current_high
         ...---|-------|--|------|--------|---...
-        
+
         Now you can buy-sell like so:
         ```
         a - current_low
@@ -92,16 +92,16 @@ class Solution:
 
         current_low = 0
         current_high = 0
-        
+
         profit = 0
-        
+
         # Pad start with `float('inf')` to trigger a fake sell of `0 - 0` on the first day.
         # This triggers the `else` clause in for-loop below.
         previous_prices = chain(
             (float('inf'),),
             prices,
         )
-        
+
         # Pad end with `-float('inf')` to trigger a sell at the very end.
         # This triggers the `else` clause in the for-loop below.
         # We need to do this because the way we track the end of an increasing subsequence
@@ -114,12 +114,12 @@ class Solution:
             prices,
             (-float('inf'),)
         )
-        
+
         for previous_price, current_price in zip(previous_prices, current_prices):
             if previous_price <= current_price: # add element to increasing subsequence
                 current_high = current_price
             else: # sell and restart increasing subsequence
                 profit += current_high - current_low
                 current_low = current_high = current_price
-                
+
         return profit
--- python/189.rotate-array.py
+++ python/189.rotate-array.py
@@ -5,7 +5,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
+
         return self.rotate4(nums, k)
 
     def rotate4(self, nums: List[int], k: int) -> None:
@@ -19,10 +19,10 @@ class Solution:
         k %= len(nums)
         if k == 0:
             return
-        
+
         i = 0
         num_elements_rotated = 0
-        
+
         while num_elements_rotated < len(nums):
             j = i
             temp = nums[j]
@@ -33,7 +33,7 @@ class Solution:
                 if i == j:
                     break
             i += 1
-    
+
     def rotate3(self, nums: List[int], k: int) -> None:
         """
         LeetCode solution 3 but modified to match `rotate0()`.
@@ -51,10 +51,10 @@ class Solution:
         k %= len(nums)
         if k == 0:
             return
-        
+
         i = 0
         num_elements_rotated = 0
-        
+
         while num_elements_rotated < len(nums):
             j = i
             while True:
@@ -66,7 +66,7 @@ class Solution:
                 # Swap from previous iteration already put correct element at index `i`.
                 nums[i], nums[j] = nums[j], nums[i]
             i += 1
-    
+
     def rotate2(self, nums: List[int], k: int) -> None:
         """
         LeetCode solution 4.
@@ -81,7 +81,7 @@ class Solution:
         nums.reverse()
         self.reverse(nums, 0, k)
         self.reverse(nums, k, len(nums))
-        
+
     def reverse(self, nums: List[int], start: int, end: int):
         length = end - start
         last_index = end - 1
@@ -89,10 +89,10 @@ class Solution:
             nums[start + offset], nums[last_index - offset] = (
                 nums[last_index - offset], nums[start + offset]
             )
-    
+
     def rotate1(self, nums: List[int], k: int) -> None:
         return self.rotate1_helper(nums, len(nums), k)
-        
+
     def rotate1_helper(self, nums: List[int], length: int, k: int) -> None:
         """
         Notice that when rotating a list, two partitions of the list maintain the same order:
@@ -105,7 +105,7 @@ class Solution:
         The way this works is by noticing that after using `swap_shift_to_end()` to
         move `length - k` elements to their correct position, the left sublist of length `k`
         is in order but just rotated.
-        
+
         For example, after running `swap_shift_to_end([1,2,3,4,5,6,7], 7, 3)`, we get
         [7,5,6,    1,2,3,4]
         `1,2,3,4` are in the correct order and positions.
@@ -115,16 +115,16 @@ class Solution:
         but just cycles through the indices as space runs out.
         We can then recursively call `rotate1_helper()`.
         We just need to generalize how to get the rotation value `2`.
-        
+
         Notice that the workspace elements were previously `4,5,6`, then became
         rotated when swapping to `7,5,6`. This means that it is unrotated every `% 3`.
         The rotation is controlled by the number of swaps done by `swap_shift_to_end()`.
-        
+
         The number of swaps is `length - k`.
         The number of swaps that over-rotated the workspace is `(length - k) % k`
         For example,
         7,5,6 is over-rotated by `(7 - 3) % 3 == 4 % 3 == 1`
-        
+
         We can reverse this rotation by doing a negative rotation:
         `- ((length - k) % k)`
         To make it positive, we can add the divisor `k`:
@@ -145,13 +145,13 @@ class Solution:
 
         self.swap_shift_to_end(nums, length, k)
         self.rotate1_helper(nums, k, -(length % k))
-            
+
     def swap_shift_to_end(self, nums: List[int], length: int, k: int) -> None:
         """
         Keep `k` elements indices at the start of the list as a workspace to swap in
         `length - k` elements to their correct location at the end of the list.
         Cycle through the workspace list to preserve order.
-        
+
         Example:
         Input: [1,2,3,4,5,6,7] and length = 7, k = 3
         Steps:
@@ -180,17 +180,17 @@ class Solution:
         """
         See Theorem 7.2 proof:
         https://www.eecs70.org/static/notes/n7.pdf
-        
+
         From this we know that if the `gcd(len(nums), k) === 1`,
         then `0, k, 2*k, ..., (len(nums) - 1)*k` are distinct `% len(nums)`.
-        
+
         If `gcd(len(nums), k) > 1`, stepping by `k` will not touch all elements.
         We need to increment the starting range `i` by `1` to reach the next set elements.
         The `gcd_nums_k` tells us how many sets of elements there are.
         Each element set will have `len(nums) // gcd_nums_k` elements.
         Note that `len(nums) // gcd_nums_k` is a whole number, since `gcd_nums_k` is a divisor
         of `len(nums)`.
-        
+
         Proof of why GCD works:
         It has to do with a cycle repeating which is the LCM(len(nums), k).
         You can then calculate the number of elements in a cycle by doing:
@@ -201,7 +201,7 @@ class Solution:
         https://stackoverflow.com/questions/23321216/rotating-an-array-using-juggling-algorithm
         https://www.chegg.com/homework-help/interesting-relationship-gcd-lcm-two-numbers-product-two-num-chapter-8.3-problem-40e-solution-9780536974105-exc
         https://proofwiki.org/wiki/Product_of_GCD_and_LCM
-        
+
         If you need to calculate `gcd()` manually, can use Euclid's algorithm in n7.pdf.
         Can also use Extended Euclid's algorithm in n7.pdf.
         Implementation:
